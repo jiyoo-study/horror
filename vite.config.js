f6de82c221b7f7c3e1bf0a09c52c249c1693c5ba
jiyoo <deepdive.jiyoo@gmail.com>
2025-08-14 11:27:45 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 깃허브 페이지스 배포를 위한 기본 경로 설정
+  base: '/horror/',
+})
